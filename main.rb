ab78b4e6bd7d827c740ac7fe9e9411a2700bc946
Alina232323 <alina.polianska@vergesense.com>
2023-06-22 20:36:51 -0700

    added user input

--- main.rb
+++ main.rb
@@ -1,6 +1,4 @@
-
-
-eu_country_capitals = {
+europe_country_capitals = {
     "Albania" => "Tirana",
     "Andorra" => "Andorra la Vella",
     "Armenia" => "Yerevan",
@@ -53,3 +51,44 @@ eu_country_capitals = {
     "United Kingdom" => "London",
     "Vatican City" => "Vatican City"
   }
+
+
+  loop do
+
+    puts "====== Welcome to the Learn European Country Capitals! ========\n   Start the game by answering the questions below!\n============== To quit, press 'q' ======================"
+    
+    questions_counter = { true => 0, false => 0 }
+    asked_countries = []
+    score = 0
+    
+    countries = europe_country_capitals.keys
+    
+    while asked_countries.length < countries.length
+        country = countries.sample
+
+        #check wether country has been asked before
+        unless asked_countries.include?(country)
+          asked_countries.push(country)
+          puts "What is the capital of #{country}?"
+          answer = gets.chomp
+    
+          break if answer.downcase == 'q'
+
+          if answer.downcase == europe_country_capitals[country].downcase
+            puts "Correct answer!"
+            questions_counter[true] += 1
+            score += 1
+            puts("Total of countries answered correctly: #{questions_counter[true]}")
+          else
+            puts "Incorrect answer!"
+            questions_counter[false] += 1
+          end
+        end
+      end
+
+    
+    
+    
+    break if input.downcase == 'q'
+  end
+
